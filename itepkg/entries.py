0bd7d902ea858dfc0c15682e8fa0d5d15f0379eb
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2017-09-02 19:53:54 +0200

    Implement MemoryEntry

--- itepkg/entries.py
+++ itepkg/entries.py
@@ -3,6 +3,36 @@
 from type.uint32 import uint32
 from type.vector import vector
 
+class MemoryEntry:
+
+    def __init__(self, address, content):
+        if isinstance(address, uint32):
+            self.address = address
+        else:
+            self.address = uint32(address, little=True)
+
+        if isinstance(content, bytes):
+            self.content = content
+        else:
+            self.content = bytes(content)
+
+    def __bytes__(self):
+        length = uint32(len(self.content), little=True)
+        return bytes(self.address) + bytes(length) + self.content
+
+    def __str__(self):
+        return '{address = 0x%x, length = %s, content = %s}' % (
+                self.address.integer, len(self.content), self.content)
+
+    def __len__(self):
+        return len(bytes(self))
+
+    def from_bytes(b):
+        address, b = uint32.from_bytes(b, little=True)
+        length, b = uint32.from_bytes(b, little=True)
+        content, b = b[:length]. b[length:]
+        return MemoryEntry(address, content), b
+
 class UnknownEntry:
 
     def __init__(self, unknown1, unknown2):
