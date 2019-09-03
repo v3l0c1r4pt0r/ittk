b61ca7818305e24551f75375e94b40999830cc0f
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2019-09-03 18:59:53 +0200

    Move content listing to handler and do
    Move code responsible for basic content listing to proper handler and
    execute action selected by user

--- itetool.py
+++ itetool.py
@@ -16,7 +16,22 @@ def do_fail(pkg):
     sys.exit(1)
 
 def do_list(pkg):
-    raise Exception('Not implemented')
+    print(' ITEPKG', end='')
+    for e in pkg.entries:
+        print('')
+        if isinstance(e, itepkg.entries.MemoryEntry):
+            print(' |- SMEDIA(',hex(e.address.integer),')', end='')
+        elif isinstance(e, itepkg.entries.DirectoryEntry):
+            print(' |- Dir(',e.filename.array[0].decode('utf-8'),')', end='')
+        elif isinstance(e, itepkg.entries.FileEntry):
+            print(' |- File(',e.filename.array[0].decode('utf-8'),')', end='')
+        elif isinstance(e, itepkg.entries.UnknownEntry):
+            print(' |- Unknown(',hex(e.unknown1.integer),hex(e.unknown2.integer),')', end='')
+        elif isinstance(e, itepkg.entries.EndEntry):
+            print(' |- End', end='')
+        else:
+            print(' |-', type(e), end='')
+    print('\r `- ')
 
 def do_unpack(pkg):
     raise Exception('Not implemented')
@@ -58,22 +73,8 @@ def main(argv):
     if (len(b) > 0):
         print("Superfluous bytes after ITEPKG. Make sure you gave proper ITEPKG file", file=sys.stderr)
 
-    print(' ITEPKG', end='')
-    for e in pkg.entries:
-        print('')
-        if isinstance(e, itepkg.entries.MemoryEntry):
-            print(' |- SMEDIA(',hex(e.address.integer),')', end='')
-        elif isinstance(e, itepkg.entries.DirectoryEntry):
-            print(' |- Dir(',e.filename.array[0].decode('utf-8'),')', end='')
-        elif isinstance(e, itepkg.entries.FileEntry):
-            print(' |- File(',e.filename.array[0].decode('utf-8'),')', end='')
-        elif isinstance(e, itepkg.entries.UnknownEntry):
-            print(' |- Unknown(',hex(e.unknown1.integer),hex(e.unknown2.integer),')', end='')
-        elif isinstance(e, itepkg.entries.EndEntry):
-            print(' |- End', end='')
-        else:
-            print(' |-', type(e), end='')
-    print('\r `- ')
+    # do selected action
+    action_handlers[action](pkg)
 
     # TODO: do stuff
 
